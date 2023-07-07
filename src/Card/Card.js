249493d3e302a8e4354e50c4d61980d8f77fc7f3
Rachel Prather <rachelprather1@gmail.com>
2023-07-06 18:55:39 -0700

    Style fonts

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,7 +4,7 @@ const Card = ({ title, rating, image, id}) => {
   return (
     <article>
       <img src={image} className="card-image" />
-      <div>
+      <div className="card-details">
         <h3>{title}</h3>
         <p>{rating}</p>
       </div>
