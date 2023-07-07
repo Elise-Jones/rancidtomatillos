223f48a189f4ef00fb081313ddb8de0ce8253281
Elise Jones <02elise.jones02@gmail.com>
2023-07-06 19:02:55 -0600

    Add files for components and begin building App and Gallery components.

---
+++ src/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+
+const Gallery = () => {
+
+  return (
+    <main className="gallery">
+      <h2>All movies</h2>
+      <section className="cards-container">CARDS</section>
+    </main>
+  )
+}
+export default Gallery
