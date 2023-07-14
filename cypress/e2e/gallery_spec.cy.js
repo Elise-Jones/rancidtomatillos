851798202d8e480ec6997def981f4f1da0f4cf41
Elise Jones <02elise.jones02@gmail.com>
2023-07-14 11:34:31 -0600

    Add additional tests for adjusted CSS

--- cypress/e2e/gallery_spec.cy.js
+++ cypress/e2e/gallery_spec.cy.js
@@ -34,11 +34,13 @@ describe("Should be able to click one movie and show additional details about th
   })
   it('Should show a back button', () => {
     cy.get('main')
-      .get('button').contains('go back')
+      .find('button').contains('Go Back')
   });
   it('Should show a list of movie details', () => {
     cy.get('main')
-      .get('span')
-      .get('.movie-details').children().should('have.length', 7)
+      .get('.movie-details').children().should('have.length', 5)
+      .get('p').first().contains('Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.')
+      .get('p').last().contains('Runtime: 125 min')
+
   })
 });
