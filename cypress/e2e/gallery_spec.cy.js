9c58ca8e792762ab0b695d7a6e04dc0118ae12fc
Elise Jones <02elise.jones02@gmail.com>
2023-07-14 13:18:26 -0600

    Add tests for URLS

--- cypress/e2e/gallery_spec.cy.js
+++ cypress/e2e/gallery_spec.cy.js
@@ -4,7 +4,7 @@ describe("Rancid Tomatillos page load behavior", () => {
     cy.loadPage();
   });
   it("Should be able to visit homepage and view title", () => {
-    cy.get('h1').contains("Rancid Tomatillos");
+    cy.get("h1").contains("Rancid Tomatillos");
   });
   it("Should be able to visit homepage and view all movies", () => {
     cy.get(".cards-container")
@@ -24,23 +24,56 @@ describe("Should be able to click one movie and show additional details about th
     cy.loadPage();
     cy.getSingleFirstView();
   });
-  it('Should show movie title', () => {
-    cy.get('main')
-      .get('h2').contains('Black Adam')
-  })
-  it('Should show movie tagline', () => {
-    cy.get('main')
-      .get('h3').contains('The world needed a hero. It got Black Adam.')
-  })
-  it('Should show a back button', () => {
-    cy.get('main')
-      .find('button').contains('Go Back')
+  it("Should show movie title", () => {
+    cy.get("main").get("h2").contains("Black Adam");
   });
-  it('Should show a list of movie details', () => {
-    cy.get('main')
-      .get('.movie-details').children().should('have.length', 5)
-      .get('p').first().contains('Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.')
-      .get('p').last().contains('Runtime: 125 min')
-
-  })
+  it("Should show movie tagline", () => {
+    cy.get("main")
+      .get("h3")
+      .contains("The world needed a hero. It got Black Adam.");
+  });
+  it("Should show a back button", () => {
+    cy.get("main").find("button").contains("Go Back");
+  });
+  it("Should show a list of movie details", () => {
+    cy.get("main")
+      .get(".movie-details")
+      .children()
+      .should("have.length", 5)
+      .get("p")
+      .first()
+      .contains(
+        "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world."
+      )
+      .get("p")
+      .last()
+      .contains("Runtime: 125 min");
+  });
 });
+
+describe("Navigation between pages", () => {
+  beforeEach(() => {
+    cy.loadPage();
+  });
+  it("Should load correct URL for homepage", () => {
+    cy.url().should("eq", "http://localhost:3000/");
+  });
+  it("When the user clicks on a movie the correct URL is shown", () => {
+    cy.getSingleFirstView();
+    cy.url().should("eq", "http://localhost:3000/436270");
+  });
+  it("When the user clicks on a the back button correct URL is shown", () => {
+    cy.getSingleFirstView().get("button").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
+  it("When the user clicks on a the Rancid Tomatillos logo correct URL is shown", () => {
+    cy.getSingleFirstView().get("button").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
+});
+
+describe("Navigation between pages", () => {
+  it("Should show error when there is network error", () => {
+    
+  })
+})
